6f189c39aa991f332d2f56ad9d8eff43a4d931dc
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-04-26 16:42:25 +0100

    added router-for-menu-and-routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+  { path: 'skills', component: SkillsPageComponent },
+  { path: 'contacts', component: ContactsPageComponent }
 ];
 
 @NgModule({
