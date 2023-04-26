16147c687a26efa9375c44b80e72fcd06e1e1f40
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-04-26 15:33:34 +0100

    created projects-page, skills-page, contacts-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ToolbarComponent,
-    FooterComponent
+    FooterComponent,
+    ProjectsPageComponent,
+    SkillsPageComponent,
+    ContactsPageComponent
   ],
   imports: [
     BrowserModule,
