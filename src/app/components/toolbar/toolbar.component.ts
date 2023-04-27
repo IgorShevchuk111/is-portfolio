63b9f546f495872fbe2024162f0837ef89b447bd
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-04-27 10:47:17 +0100

    Added to mat-slide-toggle icons

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2) { }
 
-  constructor() { }
+  @ViewChild('darkModeSwitch', { read: ElementRef }) element: ElementRef | undefined;
 
-  ngOnInit(): void {
+  checked = false;
+  disabled = false;
+  // Mat-slide-toggle
+  ngAfterViewInit() {
+    this.setIcon();
+  }
+  setIcon() {
+    if (this.element) {
+      const targetSpan: HTMLElement = this.element.nativeElement.querySelector('.mat-slide-toggle-thumb');
+      while (targetSpan.firstChild) {
+        targetSpan.firstChild.remove();
+      }
+      const elem = this.renderer.createElement('mat-icon');
+      const icon = this.checked ? 'dark_mode' : 'light_mode';
+      elem.setAttribute('class', 'mat-icon notranslate material-icons mat-icon-no-color light-mode-switch-icon left-icon');
+      elem.textContent = icon
+      targetSpan.appendChild(elem);
+    }
+  }
+  changeTheme() {
+    this.checked = !this.checked;
+    this.setIcon();
   }
 
 }
